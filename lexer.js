705087ec524ec232882c737b227bb9abfd8474bb
NitroXAce <gearheadgareth@outlook.com>
2026-02-16 21:23:45 -0500

    pushing through the lexer

--- lexer.js
+++ lexer.js
@@ -1,8 +1,154 @@
-module.exports={
-    lexer: function Lexer(){
-        this.__proto__ = null;
-        this.tokenizer = require('./modules').tokenizer;
-        this.src = arguments[0];
+function MergeObj(){
+    this.obj1 = arguments[0];
+    this.obj2 = arguments[1];
+    for(this.key in this.obj2)
+        this.obj1[this.key]=this.obj2[this.key];
+    return new KeepOnly(this,['obj1']).obj1;
+}
 
+function CondenseToken(){
+    this.buffer = arguments[0];
+    this.result = [];
+    this.i = -1;
+    this.end = this.buffer.length;
+    while(this.i++<this.end)
+        if(this.buffer[this.i] != '')
+            this.result[this.result.length]=this.buffer[this.i];
+    return new KeepOnly(this, ['result']).result;
+}
+
+function KeepOnly(){
+    this.obj = arguments[0];
+    this.keep = arguments[1];
+    this.result = {};
+    for(this.index in this.keep)
+        for(this.key in this.obj)
+            if(this.key==this.keep[this.index])
+                this.result[this.key]=this.obj[this.key];
+    delete this.obj;
+    delete this.keep;
+    delete this.index;
+    delete this.key;
+    return this.result;
+}
+
+function SplitCheck() {
+    this.stringy = arguments[0];
+    this.splitter = arguments[1];
+    this.matcher = arguments[2];
+    this.hold = [[]];
+    this.result = null;
+    
+    for (this.i in this.stringy)
+        if (this.stringy[this.i] != this.splitter)
+            this.hold[this.hold.length - 1] += this.stringy[this.i];
+        else this.hold[this.hold.length] = [];
+    
+    if (!!this.matcher) {
+        for (this.i in this.hold)
+            if (this.hold[this.i] == this.matcher)
+                return (
+                    this.result = true,
+                    new KeepOnly(this,['result']).result
+                );
+        new KeepOnly(this, ['result']);
+        this.result = false;
     }
+    
+    return new KeepOnly(this, ['hold']).hold;
+}
+
+function TokenSearch() {
+    this.obj = require('./tokenizer').tokens;
+    this.tokenArray = arguments;
+    this.tokenObject = this.obj;
+    this.tokenKept = "";
+    this.i = -1;
+    this.result = undefined;
+    
+    while (++this.i < this.tokenArray.length)
+        if (
+            typeof this.tokenObject[this.tokenArray[this.i]] != "undefined"
+        ) {
+            this.tokenKept += this.tokenArray[this.i];
+            this.tokenObject = this.tokenObject[this.tokenArray[this.i]];
+            
+            if (typeof this.tokenObject == "object") continue;
+            if (typeof this.tokenObject == "string"){
+                this.result = {
+                    length: this.tokenKept.length,
+                    token: this.tokenKept,
+                    name: this.tokenObject
+                };
+                return new KeepOnly(this, ['result']).result;
+            }
+        }
+        if (
+            typeof this.tokenObject[""] != "undefined"
+        ){
+            this.result = {
+                length: this.tokenKept.length,
+                token: this.tokenKept,
+                name: this.tokenObject[""]
+            };
+            return new KeepOnly(this, ['result']).result;    
+        }
+    return new KeepOnly(this, ['result']).result;    
+}
+
+function TokenCrammer(){
+    this.buffer = arguments[0];
+    this.token = arguments[1];
+    this.keyword = arguments[2];
+    if(new SplitCheck(this.token.name,'-',this.keyword)){
+        if(
+            typeof this.buffer[this.buffer.length-1]=='object' &&
+            new SplitCheck(this.buffer[this.buffer.length-1].name,'-',this.keyword)
+        ){
+            this.buffer[this.buffer.length-1] = this.token;
+        }
+        this.buffer[this.buffer.length]=this.token;
+    }
+    return new KeepOnly(this, ['buffer']).buffer;
+}
+
+function CharLexer(){
+    this.buffer     = [];
+    this.src        = arguments[0]||"";
+    this.end        = this.src.length;
+    this.i          = -1;
+    this.ln         = 0;
+    this.pos        = 0;
+    while(++this.i<this.end){
+        this.charToken = new TokenSearch(this.src);
+
+        if(!this.charToken){
+            this.charToken={
+                length:1,
+                name:'char',
+                token:this.src[this.i]
+            };
+            this.buffer[this.buffer.length]= this.charToken;
+            this.src.shift();
+            this.end = this.src.length;
+            continue;
+        }
+
+        this.buffer[this.buffer.length]= this.charToken;
+        this.i+=this.charToken.length;
+        for(this.j = -1; ++this.j < this.charToken.length;)
+            this.src.shift();
+        this.end = this.src.length;
+    }
+
+    
+    return new KeepOnly(this, ['buffer', 'tokenBank', 'wordBank']);
+}
+
+
+
+
+
+module.exports={
+    CharLexer, SplitCheck, KeepOnly, TokenSearch
 }
---
+++ tokenizer.js
@@ -0,0 +1,298 @@
+module.exports = {
+    tokens: {
+
+        //default "chars"
+        //'': 'char',
+
+        //whitespace operators
+        ' ': 'whitespace-space',
+        '\n': 'whitespace-new-line',
+
+        //enclosure operators
+        "(": "open-paren",
+        ")": "close-paren",
+        "[": "open-square",
+        "]": "close-square",
+        "{": "open-curly",
+        "}": "close-curly",
+        ";": "semicolon",
+        ":": "logic-colon-false",
+        '\\':'escape-character',
+        '"': "dbl-quote",
+        "'": "sgl-quote",
+        ",": "comma",
+        ".": {
+            "": "type-lex-assign-child-dot",
+            ".": {
+                "": "num-range",
+                ".": "spread-rest",
+            },
+        },
+
+        //arithmetic operators prefix
+        "+": {
+            "": "biop-math-add",
+            "=": "self-add",
+            "+": "unop-cycle-incriment",
+        },
+        "-": {
+            "": "biop-math-subtract",
+            "=": "self-subtract",
+            "-": "unop-cycle-decriment",
+        },
+        "*": {
+            "": "biop-multiply",
+            "=": "self-multiply",
+            "*": "biop-power",
+            "/": "close-comment",
+        },
+        "/": {
+            "": "biop-divide",
+            "=": "self-divide",
+            "/": "unop-line-comment",
+            "*": "open-comment",
+        },
+        "%": {
+            "": "biop-modulus",
+            "=": "self-modulus",
+        },
+
+        //comparison operators
+        "=": {
+            "": "assign",
+            "=": "biop-logic-strict-equal",
+        },
+        "!": {
+            "": "unop-logic-not",
+            "=": "biop-logic-not-equal",
+            "!": "unop-logic-absolute-boolean",
+        },
+        "?": {
+            "": "logic-ternary-true",
+            "?": "biop-logic-nullish",
+            ".": "biop-logic-optional-chain",
+        },
+        ">": {
+            "": "biop-greater-than",
+            "=": "greater-than-equal",
+            ">": "biop-shift-right",
+        },
+        "<": {
+            "": "biop-less-than",
+            "=": "less-than-equal",
+            "<": "biop-shift-left",
+        },
+        "&": {
+            "": "biop-bit-and",
+            "=": "self-bit-and",
+            "&": "biop-logic-and",
+        },
+        "|": {
+            "": "unop-bit-or",
+            "=": "self-bit-or",
+            "|": "biop-logic-or",
+        },
+        "^": {
+            "": "biop-bit-xor",
+            "=": "self-bit-xor",
+        },
+    //},
+    //words: {
+        /*//types
+        const: "type-lex-assign-constant",
+        bool: "type-lex-assign-boolean",
+        num: "type-lex-assign-number",
+        str: "type-lex-assign-string",
+        obj: "type-lex-assign-object",
+        arr: "type-lex-assign-array",
+        fn: "type-lex-assign-function",
+        void: "type-lex-assign-void",
+        super: "type-lex-assign-superset",
+        import: "type-lex-module-import",
+        export: "type-lex-module-export",
+
+        //scope words
+        if: "scope-lex-if",
+        else: "scope-lex-else",
+        while: "scope-lex-while",
+        in: "scope-lex-in",
+        of: "scope-lex-of",
+
+        //escape words
+        return: "scope-exit-return",
+        break: "scope-exit-break",
+        continue: "scope-exit-continue",*/
+
+        /*let words = {
+            arr: "type-lex-assign-array",
+            bool: "type-lex-assign-boolean",
+            break: "scope-exit-break",
+            const: "type-lex-assign-constant",
+            continue: "scope-exit-continue",
+            else: "scope-lex-else",
+            export: "type-lex-module-export",
+            fn: "type-lex-assign-function",
+            if: "scope-lex-if",
+            import: "type-lex-module-import",
+            in: "scope-lex-in",
+            num: "type-lex-assign-number",
+            obj: "type-lex-assign-object",
+            of: "scope-lex-of",
+            return: "scope-exit-return",
+            str: "type-lex-assign-string",
+            super: "type-lex-assign-superset",
+            void: "type-lex-assign-void",
+            while: "scope-lex-while"
+        };*/
+
+        //numbers - because WHY NOT
+        "0": "num-literal-bin-octal-dec-hex",
+        "1": "num-literal-bin-octal-dec-hex",
+        "2": "num-literal-octal-dec-hex",
+        "3": "num-literal-octal-dec-hex",
+        "4": "num-literal-octal-dec-hex",
+        "5": "num-literal-octal-dec-hex",
+        "6": "num-literal-octal-dec-hex",
+        "7": "num-literal-octal-dec-hex",
+        "8": "num-literal-dec-hex",
+        "9": "num-literal-dec-hex",
+
+        //brute force matching: like tokens but for words
+        a:{
+            '':'letter-num-literal-hex',
+            r:{
+                '':'letter',
+                r:"type-lex-assign-array"
+            }
+        },
+        b:{
+            '':'letter-num-literal-hex',
+            o:{
+                '':'letter',
+                o:{
+                    '':'letter',
+                    l:"type-lex-assign-boolean"
+                }
+            },
+            r:{
+                '':'letter',
+                e:{
+                    '':'letter',
+                    a:{
+                        '':'letter',
+                        k:"scope-exit-break"
+                    }
+                }}
+        },
+        c:{
+            '':'letter-num-literal-hex',
+            o:{
+                '':'letter',
+                n:{
+                    '':'letter',
+                    s:{
+                        '':'letter',
+                        t:"type-lex-assign-constant"
+                    }},
+                    t:{
+                        '':'letter',
+                        i:{
+                            '':'letter',
+                            n:{
+                                '':'letter',
+                                u:{
+                                    '':'letter',
+                                    e:"type-lex-assign-assign-continue"
+                                }}}
+        }}},
+        d:'letter-num-literal-hex',
+        e:{
+            '':'letter-num-literal-hex',
+            l:{
+                '':'letter',
+                s:{
+                    '':'letter',
+                    e:"scope-lex-else"
+                }},
+            x:{
+                '':'letter',
+                p:{
+                    '':'letter',
+                    o:{
+                        '':'letter',
+                        r:{
+                            '':'letter',
+                            t:"type-lex-module-export"
+                        }}}}
+        },
+        f:{
+            '':'letter-num-literal-hex',
+            n:"type-lex-assign-function"
+        },
+        i:{
+            '':'letter',
+            f:"word-lex-identifier-function",
+            n:"scope-lex-in",
+            m:{p:{o:{r:{t:"type-lex-module-import"}}}},
+        },
+        n:{
+            '':'letter',
+            u:{
+                '':'letter',
+                m:"type-lex-assign-number"
+            },
+        o:{
+            '':'letter',
+            b:{
+                '':'letter',
+                j:"type-lex-assign-object"
+            },
+            f:"scope-lex-of"           
+        },
+        r:{
+            '':'letter',
+            e:{
+                '':'letter',
+                t:{
+                    '':'letter',
+                    u:{
+                        '':'letter',
+                        r:{
+                            '':'letter',
+                            n:"scope-exit-return"
+        }}}}}},
+        s:{
+            '':'letter',
+            t:{
+                '':'letter',
+                r:"type-lex-assign-string"
+            },
+            u:{
+                '':'letter',
+                p:{
+                    '':'letter',
+                    e:{
+                        '':'letter',
+                        r:"type-lex-assign-superset"
+                    }}}
+        },
+        v:{
+            '':'letter',
+            o:{
+                '':'letter',
+                i:{
+                    '':'letter',
+                    d:"type-lex-assign-void"
+        }}},
+        w:{
+            '':'letter',
+            h:{
+                '':'letter',
+                i:{
+                    '':'letter',
+                    l:{
+                        '':'letter',
+                        e:"scope-lex-while"
+        }}}}
+    },
+};
